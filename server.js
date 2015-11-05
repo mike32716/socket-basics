212ee4613e340d673f057891d40f4f1bbfcac0ab
Michael Hansen <mike32716@gmail.com>
2015-11-05 18:13:23 -0500

    Added custom message event

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ var app = express();  //just call express no args
 var http = require('http').Server(app);  //Anything the express app listens to the server should also.
 var io = require('socket.io')(http);
 
- app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(){        //listen for events
-  console.log('User connected via socket.io!');
+io.on('connection', function(socket){        //listen for events
+
+    console.log('User connected via socket.io!');
+
+    socket.on('message', function(message){
+        console.log('Message received: ' + message.text);
+
+        socket.broadcast.emit('message', message);
+    });
+
+    socket.emit('message', {    //only get one arg.  Use an object to hold a bunch of stuff
+        text: 'Welcome to the chat application'
+    });
+
 });
 
 
