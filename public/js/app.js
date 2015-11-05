4804d1b6c8b3b8e8b6a917a071dee01d7ddb59f8
Michael Hansen <mike32716@gmail.com>
2015-11-05 18:57:21 -0500

    added input box to front end and get the data from backend

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,26 @@
 var socket = io();
 
 socket.on('connect', function(){
-
     console.log('Connected to socket.io server!!!');
-
 });
+
 
 socket.on('message', function(message){
     console.log('New message:');
     console.log(message.text);
-})
+});
+
+
+// Handles submitting of new message
+var $form = jQuery('#message-form');   // only use $ to say it is jquer element
+//console.log($form);
+
+$form.on('submit', function(event){
+    event.preventDefault();
+
+    socket.emit('message', {
+            text: $form.find('input[name=message]').val()
+              });
+
+    $form.find('input[name=message]').val('');  //delete input box after submit
+});
